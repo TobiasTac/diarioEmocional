0920b34a5425c3a2bf2e01842973ccdbffe54968
Tobias Alves <tobias.vida@live.com>
2023-10-15 19:00:03 -0300

    first commit

---
+++ modal.php
@@ -0,0 +1,62 @@
+<!-- create  -->
+
+<div class="modalCreate">
+  <div class="modalCreate-container">
+    <div class="modalCreate-header">
+      <h2> Criar Novo Diário &#43;  </h2>
+      <button class="modalCreateClose"> &times; </button>
+    </div>
+    <div class="modalCreate-content">
+      <h1> Título: </h1>
+      <input type="text" placeholder="Escreva Aqui...">
+      <h1> Conte Seu Dia: <h1>
+      <textarea name="" id="" cols="30" rows="10" placeholder="Escreva Aqui..."></textarea>
+    </div>
+    <div class="modalCreate-footer">
+      <button class="button"> Save </button>
+      <button class="button delete"> Cancel </button>
+    </div>
+  </div>
+</div>
+
+<!-- read more -->
+
+<div class="modalRead">
+  <div class="modalRead-container">
+    <div class="modalRead-header">
+      <h2> Criar Novo Diário &#43;  </h2>
+      <button class="modalReadClose"> &times; </button>
+    </div>
+    <div class="modalRead-content">
+      <h1> Título: </h1>
+      <input type="text" placeholder="Escreva Aqui..." value="$title">
+      <h1> Conte Seu Dia: <h1>
+      <textarea name="" id="" cols="30" rows="10" placeholder="Escreva Aqui..."> $cont </textarea>
+    </div>
+    <div class="modalRead-footer">
+      <button class="button"> Save </button>
+      <button class="button delete"> Cancel </button>
+    </div>
+  </div>
+</div>
+
+<!-- filter -->
+
+<div class="modalRead">
+  <div class="modalRead-container">
+    <div class="modalRead-header">
+      <h2> Criar Novo Diário &#43;  </h2>
+      <button class="modalReadClose"> &times; </button>
+    </div>
+    <div class="modalRead-content">
+      <h1> Título: </h1>
+      <input type="text" placeholder="Escreva Aqui..." value="$title">
+      <h1> Conte Seu Dia: <h1>
+      <textarea name="" id="" cols="30" rows="10" placeholder="Escreva Aqui..."> $cont </textarea>
+    </div>
+    <div class="modalRead-footer">
+      <button class="button"> Save </button>
+      <button class="button delete"> Cancel </button>
+    </div>
+  </div>
+</div>
---
+++ test.php
@@ -0,0 +1,84 @@
+<div class="container">
+      <section class="route">
+            <div class="items">
+                  <a href="#">
+                        <i class="bx bxs-home"></i>
+                  </a>
+
+                  >
+
+                  <a href="#" class="text sub">
+                        <div>Diário Emocional</div>
+                  </a>
+            </div>
+      </section>
+      <section class="func">
+            <div class="seacreate">
+                  <i class="bx bx-search icon-search"></i>
+                  <input type="text" placeholder="Search">
+                  <a href="#" class="filter"><i class='bx bx-filter-alt icon-search filter'></i></a>
+            </div>
+            <div class="createDiario">
+                  <button class="button crDiario"> Criar Novo Diário +</button>
+            </div>
+      </section>
+      <section class="cont-card">
+            <div class="card">
+                  <div class="icon">
+                        <img src="assets/img/Diary-amico.svg" alt="">
+                  </div>
+                  <div class="content">
+                        <h3>Title</h3>
+                        <h1>Data de Criação: 03/10/2023</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quas aspernatur? Accusantium praesentium neque non asperiores fugit repudiandae unde et at, fuga consequuntur vel quae tempora reiciendis quibusdam optioadwa. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil non reiciendis ipsa itaque maiores eveniet dicta laudantium, aliquam, tempora accusantium officia consectetur eum perferendis quos, recusandae sed explicabo ullam? Rerum!</p>
+                  </div>
+                  <div class="bt">
+                        <button class="button read"> Read More </button>
+                        <button class="button delete"> Delete </button>
+                  </div>
+            </div>
+            <div class="card">
+                  <div class="icon">
+                        <img src="assets/img/Diary-amico.svg" alt="">
+                  </div>
+                  <div class="content">
+                        <h3>Title</h3>
+                        <h1>Data de Criação: 03/10/2023</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quas aspernatur? Accusantium praesentium neque non asperiores fugit repudiandae unde et at, fuga consequuntur vel quae tempora reiciendis quibusdam optioadwa. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil non reiciendis ipsa itaque maiores eveniet dicta laudantium, aliquam, tempora accusantium officia consectetur eum perferendis quos, recusandae sed explicabo ullam? Rerum!</p>
+                  </div>
+                  <div class="bt">
+                        <button class="button read"> Read More </button>
+                        <button class="button delete"> Delete </button>
+                  </div>
+            </div>
+            <div class="card">
+                  <div class="icon">
+                        <img src="assets/img/Diary-amico.svg" alt="">
+                  </div>
+                  <div class="content">
+                        <h3>Title</h3>
+                        <h1>Data de Criação: 03/10/2023</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quas aspernatur? Accusantium praesentium neque non asperiores fugit repudiandae unde et at, fuga consequuntur vel quae tempora reiciendis quibusdam optioadwa. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil non reiciendis ipsa itaque maiores eveniet dicta laudantium, aliquam, tempora accusantium officia consectetur eum perferendis quos, recusandae sed explicabo ullam? Rerum!</p>
+                  </div>
+                  <div class="bt">
+                        <button class="button read"> Read More </button>
+                        <button class="button delete"> Delete </button>
+                  </div>
+            </div>
+            <div class="card">
+                  <div class="icon">
+                        <img src="assets/img/Diary-amico.svg" alt="">
+                  </div>
+                  <div class="content">
+                        <h3>Title</h3>
+                        <h1>Data de Criação: 03/10/2023</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quas aspernatur? Accusantium praesentium neque non asperiores fugit repudiandae unde et at, fuga consequuntur vel quae tempora reiciendis quibusdam optioadwa. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil non reiciendis ipsa itaque maiores eveniet dicta laudantium, aliquam, tempora accusantium officia consectetur eum perferendis quos, recusandae sed explicabo ullam? Rerum!</p>
+                  </div>
+                  <div class="bt">
+                        <button class="button read"> Read More </button>
+                        <button class="button delete"> Delete </button>
+                  </div>
+            </div>
+
+      </section>
+</div>
